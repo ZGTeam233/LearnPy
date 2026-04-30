71964fa745131df6f6b4ebb9ed887281f047ac6b
Zilin Zheng <wsnzg6@outlook.com>
2026-04-30 16:56:23 +0800

    -

--- src/learn-tkinter/tk3.py
+++ src/learn-tkinter/tk3.py
@@ -1,17 +1,17 @@
 #设计基本框架
 import tkinter as tk
 import tkinter.messagebox
-win=tk.Tk() #生成一个主窗口对象
-win.title("用户登录")
-win.geometry('500x260') #窗口大小
-#功能代码开始
 
-#设计功能函数
+win=tk.Tk() #生成一个主窗口对象
+
 #设置变量
 var_Name=tk.StringVar()
 var_Name.set("")
 var_Pwd=tk.StringVar()
 var_Pwd.set("")
+
+#设计功能函数
+
 #按钮处理函数
 def login():
     name=var_Name.get()
@@ -26,29 +26,37 @@ def cancel():
 def _quit():
     win.destroy()
 
-#登录窗口各组件设计
-#设计两个提示标签
-labname=tk.Label(win,text="账号",width=160)
-labpwd=tk.Label(win,text="密码",width=160)
-#设计两个输入框
-#textvariable为文本框的值
-#并关联变量var_Name
-entname=tk.Entry(win,width=200,textvariable=var_Name)
-entpwd=tk.Entry(win,show="*",width=200,textvariable=var_Pwd)
-#设计三个按钮
-but_Ok=tk.Button(win,text="登录",command=login)
-but_Cancel=tk.Button(win,text="重置",command=cancel)
-but_quit=tk.Button(win,text="退出",command=_quit)
+def main():
+    win.title("用户登录")
+    win.geometry('500x260') #窗口大小
+    #功能代码开始
 
-#登录窗口各部件布局
-#部件的窗口布局
-labname.place(x=40,y=20,width=160,height=40)
-labpwd.place(x=40,y=80,width=160,height=40)
-entname.place(x=240,y=20,width=160,height=40)
-entpwd.place(x=240,y=80,width=160,height=40)
-but_Ok.place(x=60,y=160,width=100,height=40)
-but_Cancel.place(x=200,y=160,width=100,height=40)
-but_quit.place(x=340,y=160,width=100,height=40)
+    #登录窗口各组件设计
+    #设计两个提示标签
+    labname=tk.Label(win,text="账号",width=160)
+    labpwd=tk.Label(win,text="密码",width=160)
+    #设计两个输入框
+    #textvariable为文本框的值
+    #并关联变量var_Name
+    entname=tk.Entry(win,width=200,textvariable=var_Name)
+    entpwd=tk.Entry(win,show="*",width=200,textvariable=var_Pwd)
+    #设计三个按钮
+    but_Ok=tk.Button(win,text="登录",command=login)
+    but_Cancel=tk.Button(win,text="重置",command=cancel)
+    but_quit=tk.Button(win,text="退出",command=_quit)
 
-#功能代码结束
-win.mainloop() #进入消息循环
+    #登录窗口各部件布局
+    #部件的窗口布局
+    labname.place(x=40,y=20,width=160,height=40)
+    labpwd.place(x=40,y=80,width=160,height=40)
+    entname.place(x=240,y=20,width=160,height=40)
+    entpwd.place(x=240,y=80,width=160,height=40)
+    but_Ok.place(x=60,y=160,width=100,height=40)
+    but_Cancel.place(x=200,y=160,width=100,height=40)
+    but_quit.place(x=340,y=160,width=100,height=40)
+
+    #功能代码结束
+    win.mainloop() #进入消息循环
+
+if __name__ == "__main__":
+    main()
